6095cae486a90982593324a4b60cc537d747f509
Lindo-Dlamini <lindokuhledlamini85@gmail.com>
2023-04-11 11:21:50 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - prints string of text
+ *
+ *@s: string to be printed
+ *
+ *Return: Always 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+
+		putchar(*s);
+	s++;
+	_puts_rescursion(s);
+}
+
